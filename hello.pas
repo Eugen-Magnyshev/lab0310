71c4ee95ee4922d9f8cafcff305e79360b30f587
Евгений Магнышев <magnysevevgenij@gmail.com>
2024-10-03 10:16:22 +0300

    Add cases of time

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-  Writeln('Привет, мир!');
+  var time:= ReadInteger('Введите текущий час (0-23): ');
+  Assert(time in 0..23);
+  
+  case time of
+    4..10: Writeln('Доброе утро, мир!');
+    11..16: Writeln('Добрый день, мир!');
+    17..22: Writeln('Добрый вечер, мир!');
+  else Writeln('Доброй ночи, мир!');
+  end;
 end.
