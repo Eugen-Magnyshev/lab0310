c78a41c8f9538212a4a15370d96164775de075f2
Евгений Магнышев <magnysevevgenij@gmail.com>
2024-10-05 00:08:07 +0300

    Add timer

---
+++ timer.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var year:= ReadInteger('Введите год: ');
+  
+  if year mod 4 = 0 then
+    if (year mod 100 = 0) and (year mod 400 <> 0) then
+      Writeln('Год не високосный')
+    else
+      Writeln('Год високосный')
+  else
+    Writeln('Год не високосный');
+end.
